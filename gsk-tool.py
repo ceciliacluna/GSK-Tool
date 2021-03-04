842d935d70f66fb1171c1fe736fcfb9840d16c2c
ceciliacluna <cluna@daelightsolutions.com>
2021-03-04 17:45:06 -0600

    Added left template options to DataFrame

--- gsk-tool.py
+++ gsk-tool.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Label, Frame, Entry, Button, END, TOP, HORIZONTAL, BOTTOM, S, E, NSEW
+from tkinter import Tk, Label, Frame, Entry, OptionMenu, Button, END, TOP, HORIZONTAL, BOTTOM, W, S, E, NSEW, StringVar
 from tkinter.ttk import Progressbar
 from tkinter.messagebox import showerror
 import tkinter.filedialog as filedialog
@@ -14,7 +14,7 @@ class GSKTool:
         self.master = master
         master.title('GSK Sections Tool')
 
-        self.complete_dict = {
+        self.complete_consumer = {
             "Agreement": ["Assessment", "Form", "Plan", "Policy_and_Procedure", "Protocol", "Quality_and_Audit",
                           "Record", "Reference", "Report", "Specification", "Template", "Training"],
             "Field2": [],
@@ -26,6 +26,22 @@ class GSKTool:
             "Field8": [],
             "Classification": [],
         }
+        self.complete_enterprise = {
+            "Agreement": ["Assessment", "Form", "Operations", "Plan", "Policy_and_Procedure", "Protocol",
+                          "Quality_and_Audit","Record", "Reference", "Report", "Specification", "Template",
+                          "Training"],
+            "Field2": [],
+            "Field3": [],
+            "Field4": [],
+            "Yes": ["No"],
+            "Field6": [],
+            "Restricted": ["Unrestricted"],
+            "Field8": [],
+            "Field9": [],
+            "Classification": [],
+        }
+        self.selected_from_dropdown = StringVar()
+        self.selected_from_dropdown.set("Consumer")
 
         # MAIN SCREEN
         self.input_frame = Frame(master)
@@ -42,6 +58,8 @@ class GSKTool:
         self.password_input = Label(self.input_frame, text="password:      ")
         self.password_entry = Entry(self.input_frame, text="", width=40, show="*")
 
+        self.dropdown = OptionMenu(self.top_frame, self.selected_from_dropdown, "Consumer", "Enterprise")
+
         self.file_name_input = Label(self.top_frame, text="Output file name:      ")
         self.file_name_entry = Entry(self.top_frame, text="", width=40)
 
@@ -49,16 +67,16 @@ class GSKTool:
         self.input_entry = Entry(self.top_frame, text="", width=40)
         self.browse1 = Button(self.top_frame, text="Browse", command=self.input_location)
 
-        self.output_path_label = Label(self.bottom_frame, text="Output Location:      ")
-        self.output_entry = Entry(self.bottom_frame, text="", width=40)
-        self.browse2 = Button(self.bottom_frame, text="Browse", command=self.output_location)
+        self.output_path_label = Label(self.top_frame, text="Output Location:      ")
+        self.output_entry = Entry(self.top_frame, text="", width=40)
+        self.browse2 = Button(self.top_frame, text="Browse", command=self.output_location)
 
         self.login_button = Button(self.input_frame, text='Login', command=self.authenticate)
         self.log_in_successful = Label(self.input_frame, text="Login Successful", fg="blue", font="Helvetica 9 bold", pady=6)
 
         self.progress = Progressbar(master, orient=HORIZONTAL, length=100, mode='indeterminate')
 
-        self.begin_button = Button(self.bottom_frame, text='Begin!', command=self.being_taks)
+        self.begin_button = Button(self.bottom_frame, text='Begin!', command=self.being_task)
 
         self.completed = Label(master, text="Download Complete", fg="green", font="Helvetica 10 bold", pady=6)
 
@@ -81,18 +99,20 @@ class GSKTool:
 
         self.login_button.grid(row=4, column=1, pady=5, padx=(30, 90), sticky=NSEW)
 
+        self.dropdown.grid(row=0, column=2, padx=10, sticky=E)
+
         self.file_name_input.grid(row=0, column=0, pady=5, sticky=E)
         self.file_name_entry.grid(row=0, column=1, pady=5)
 
         self.input_path_label.grid(row=1, column=0, pady=5, sticky=E)
         self.input_entry.grid(row=1, column=1, pady=5, sticky=E)
-        self.browse1.grid(row=1, column=2, padx=10, sticky=E)
+        self.browse1.grid(row=1, column=2, padx=(35, 10), sticky=W)
 
         self.output_path_label.grid(row=2, column=0, pady=5, sticky=E)
         self.output_entry.grid(row=2, column=1, pady=5, sticky=E)
-        self.browse2.grid(row=2, column=2, padx=10, sticky=E)
+        self.browse2.grid(row=2, column=2, padx=(35, 10), sticky=W)
 
-        self.begin_button.grid(row=3, column=1, ipadx=10, pady=10, padx=(10, 30), sticky=NSEW)
+        self.begin_button.grid(row=3, column=1, ipadx=40, pady=10, padx=(20, 30), sticky=NSEW)
 
     # Prompts user to select input file
     def input_location(self):
@@ -102,7 +122,6 @@ class GSKTool:
         print(data)
         self.input_entry.delete(1, END)  # Remove current text in entry
         self.input_entry.insert(0, input_path)  # Insert the 'path'
-        print("input_location")
 
     # Prompts user to select output location
     def output_location(self):
@@ -110,7 +129,6 @@ class GSKTool:
         output_path = filedialog.askdirectory()
         self.output_entry.delete(1, END)  # Remove current text in entry
         self.output_entry.insert(0, output_path)  # Insert the 'path'
-        print("output location")
 
     # Authenticate
     def authenticate(self):
@@ -141,9 +159,8 @@ class GSKTool:
             showerror(title="Error", message=msg)
             print(msg)
             raise MyCustomAPIError(msg)
-        print("authenticated")
 
-    def being_taks(self):
+    def being_task(self):
         threading.Thread(target=self.data_automation).start()
 
     # Makes API request and generates output file
@@ -161,7 +178,6 @@ class GSKTool:
         }
 
         self.progress.pack(side=BOTTOM, anchor=S, pady=10)
-        print("before for loop")
         collected_data = {}
         for objects in data['objects']:
             self.progress.step()
@@ -174,19 +190,21 @@ class GSKTool:
             for x in json_parse:
                 attribute = x['name__v']
                 collected_data.setdefault(object_name, []).append(attribute)
-        print("after for loop")
+
+        selected_option = self.selected_from_dropdown.get()
+
         # add left template to table
-        self.complete_dict.update(collected_data)
+        if selected_option == "Consumer":
+            self.complete_consumer.update(collected_data)
+            new_output = pd.DataFrame.from_dict(self.complete_consumer, orient='index')
+        else:
+            self.complete_enterprise.update(collected_data)
+            new_output = pd.DataFrame.from_dict(self.complete_enterprise, orient='index')
 
-        keys = list(self.complete_dict.keys())
-        print(keys)
-
-        new_output = pd.DataFrame.from_dict(self.complete_dict, orient='index')
         new_output = new_output.transpose()
         new_output.to_csv(output_path + '/' + file_name + '.csv', index=False)
         self.completed.pack()
         self.progress.pack_forget()
-        print("data automation")
 
 
 class MyCustomAPIError(requests.exceptions.HTTPError):
